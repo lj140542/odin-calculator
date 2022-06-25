69f120d44c0708df050c3b3a41d25aa8e186d316
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-06-25 17:40:33 +0200

    Creates the Calculator class with the basic methods needed for the project.

---
+++ calculator.js
@@ -0,0 +1,18 @@
+function Calculator() {
+
+    this.add = function (x, y) {
+        return x + y;
+    };
+
+    this.substract = function (x, y) {
+        return x - y;
+    };
+
+    this.multiply = function (x, y) {
+        return x * y;
+    };
+
+    this.divide = function (x, y) {
+        return x / y;
+    };
+};
