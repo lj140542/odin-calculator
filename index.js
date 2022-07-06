2a684f66c8f9883bda7ba6d1286f8049954301c1
lj140542 <jonathan.lefebvre76600@gmail.com>
2022-07-06 21:35:53 +0200

    Makes the calculator work.

--- index.js
+++ index.js
@@ -4,13 +4,59 @@ const calc = new Calculator();
 const display = document.getElementById('display');
 const operands = Array.from(document.getElementsByClassName('operand'));
 const operators = Array.from(document.getElementsByClassName('operator'));
+const functions = Array.from(document.getElementsByClassName('function'));
+const clearButton = document.getElementById('clear');
+
+let tabOperands = [];
+let tabOperators = [];
+let tabHisto = [];
+
 
 function operandPressed() {
+    if (tabOperators.length > 0 && display.innerHTML == tabOperands[tabOperands.length - 1]) {
+        display.innerHTML = '';
+    }
     display.innerHTML += this.textContent;
 };
+
 function operatorPressed() {
-    display.innerHTML = '';
+    if (isNaN(parseInt(display.innerHTML)) == false) {
+        tabOperands.push(parseInt(display.innerHTML));
+        tabOperators.push(this.textContent);
+        checkForOperate(true);
+    }
 };
+
+function checkForOperate(isEqual = false) {
+    let result;
+
+    if (tabOperators.length > 0 && tabOperands.length >= 2) {
+        result = calc.operate(tabOperators[0], tabOperands[0], tabOperands[1]);
+        display.innerHTML = result;
+
+        tabHisto.push('' + tabOperands[0] + ' ' + tabOperators[0] + ' ' + tabOperands[1] + ' = ' + result);
+        tabOperands[1] = result;
+    }
+
+    if (tabOperators[tabOperators.length - 1] == '=') {
+        tabOperands = [];
+        tabOperators = [];
+    } else {
+        tabOperands[0] = tabOperands.pop();
+        tabOperators[0] = tabOperators.pop();
+    }
+
+    console.log(tabHisto.toString());
+};
+
+function clear() {
+    tabOperands = [];
+    tabOperators = [];
+    tabHisto = [];
+    display.innerHTML = '';
+    clearButton.innerHTML = 'AC';
+};
+
 
 operands.forEach(element => {
     element.addEventListener('click', operandPressed);
@@ -18,4 +64,10 @@ operands.forEach(element => {
 operators.forEach(element => {
     element.addEventListener('click', operatorPressed);
 });
-
+functions.forEach(element => {
+    switch (element.id) {
+        case 'clear':
+            element.addEventListener('click', clear);
+            break;
+    }
+});
